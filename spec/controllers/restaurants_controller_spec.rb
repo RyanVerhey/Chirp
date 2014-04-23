cf0c40d756bbef2517a944dca710eb68d900f9d4
Jake Nations <jnations1214@gmail.com>
2014-04-23 17:43:19 -0500

    all get routes passed tests

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -2,7 +2,36 @@ require 'spec_helper'
 
 describe RestaurantsController do
   describe "GET #index" do
-    it "populates an array of restaurants"
-    it "renders the :index view"
+    it "should not be nil"do
+      FactoryGirl.build(:restaurant)
+      get :index
+      assigns(:restaurants).should_not be nil
+    end
+
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #Show" do
+    it "assigns the requested restaurant to @restaurant" do
+      restaurant = FactoryGirl.create(:restaurant)
+      get :show, id: restaurant
+      assigns(:restaurant).should eq(restaurant)
+    end
+
+    it "renders the #Show view" do
+      get :show, id: FactoryGirl.create(:restaurant)
+      response.should render_template :show
+    end  
+  end
+
+  describe "GET #New" do
+    it "should create a new instance of a restaurant" do
+      rest = FactoryGirl.build(:restaurant)
+      get :new    
+      assigns(:restaurant).should be_an_instance_of Restaurant
+    end
   end
 end
