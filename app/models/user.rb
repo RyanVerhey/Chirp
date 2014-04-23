8ec7a4af54f780ebc3b0a28aa3842722c381fcea
Jake Nations <jnations1214@gmail.com>
2014-04-23 10:47:36 -0500

    User migration and model complete

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true, format: { with: ConstantData::VALID_EMAIL_REGEX }
+  validates :username, presence: true, uniqueness: true 
+  has_many :restaurants
+  has_many :reviews
+end
