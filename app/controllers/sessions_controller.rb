d5970b4cde81ba16d1bcf4284c326638b2130dbe
Ryan Verhey <ryan90v@gmail.com>
2014-04-23 14:55:19 -0500

    Added login and logout methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def login
+    @user = User.where(email: user_params[:email]).first
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to :back
+    else
+      redirect_to login_path # This should be changed to whatever the login route is
+    end
+  end
+
+  def logout
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
