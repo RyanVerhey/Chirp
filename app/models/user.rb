5e562fc0f22f17045420ef834937e86a43c4dba3
Ryan Verhey <ryan90v@gmail.com>
2014-04-23 16:53:09 -0500

    Added a validation for blank passwords and a simple regex for validating emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true#, format: { with: ConstantData::VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+\.\w{2,}/ }
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
   has_many :restaurants
   has_many :reviews
   has_secure_password
