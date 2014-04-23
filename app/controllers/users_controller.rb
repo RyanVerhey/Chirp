bed9f7c2069d49cff5a254820a1799be9dc0271e
Ryan Verhey <ryan90v@gmail.com>
2014-04-23 16:06:10 -0500

    Added 'users#new' method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,29 @@ class UsersController < ApplicationController
   def login
     @user = User.new
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(first_name: user_params[:first_name],
+                     last_name: user_params[:last_name],
+                     username: user_params[:username],
+                     email: user_params[:email],
+                     password: user_params[:password],
+                     password_confirmation: user_params[:password_confirm])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirm)
+  end
 end
