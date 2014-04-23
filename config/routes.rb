56c53b79a1f4a30746da0fc2bfb2bfd3e798c3a0
Ryan Verhey <ryan90v@gmail.com>
2014-04-23 15:24:55 -0500

    Added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
 # root path
 root to: "restaurants#index"
 
+# Sessions and Users
+# post '/sessions/login' => 'sessions#login'
+get '/sessions/logout' => 'sessions#logout'
+
+get '/users/login' => 'users#login'
+
+resources :sessions, only: 'new'
+resources :users
+
 end
