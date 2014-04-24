8af35e8c061fa8212de0cf988057284e1bf406b3
Christoph Wong <wong.christoph@gmail.com>
2014-04-24 14:40:35 -0500

    made create review logic that matches restaurant and uses session userid to identify user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
-  validates :reviewer, presence: true, uniqueness: true
-  validates :restaurant, presence: true, uniqueness: true
+  # validates :reviewer, presence: true, uniqueness: true
+  validates :restaurant, presence: true
 
   belongs_to :reviewer, class_name: 'User'
   belongs_to :restaurant
