d8f9c3caaaf157e7200e97499e95de2a22a790de
Ryan Verhey <ryan90v@gmail.com>
2014-04-24 15:42:26 -0500

    Did the same thing to the new user page. Might never need it though because of Rails' built in validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:error] = "Something went wrong with creating your account. Please verify your information and try again."
       redirect_to new_user_path
     end
   end
