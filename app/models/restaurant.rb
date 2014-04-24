c09db589f3a4283df2b73d2ecae1f9008cfaa8f9
Ryan Verhey <ryan90v@gmail.com>
2014-04-24 10:28:32 -0500

    Added tag association for restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
+  has_many :categories
+  has_many :tags, through: :categories
   belongs_to :owner, class_name: "User"
   validates :name, presence: true
 end
