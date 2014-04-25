bf06a9d038d5bd75246cf2e54ad579c6740e32ff
Ryan Verhey <ryan90v@gmail.com>
2014-04-25 16:29:44 -0500

    Added associations with reviews and restaurants

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :email, uniqueness: true
   validates :email, presence: true
   validates :email, format: { with: /\w+\@\w+\.\w{2,}/ }
-  has_secure_password
+
+  has_many :reviews, foreign_key: :reviewer_id
+  has_many :restaurants, foreign_key: :owner_id
+
 end
