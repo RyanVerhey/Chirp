2dc912cfecee7207d2196b526d3270c69a8f6a85
Ryan Verhey <ryan90v@gmail.com>
2014-04-25 18:20:49 -0500

    Added JS for Google maps integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function initMaps() {
+  if($("#lat").text() === "") {
+    var myLatLng = new google.maps.LatLng(39.060027, -95.952368)
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 3
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+    } else {
+    var myLatLng = new google.maps.LatLng(parseFloat($("#lat").text()), parseFloat($("#lon").text()))
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: $("#name").text()
+    })
+  }
+}
