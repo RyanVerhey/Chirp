1650d14411fbe71ca25d91cd15d4a0f5af30ea47
Ryan Verhey <ryan90v@gmail.com>
2014-04-25 15:24:22 -0500

    Added tests and model for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ActiveRecord::Base
-
+  validates :name, presence: true
+  has_many :categories
+  has_many :restaurants, through: :categories
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe Tag do
 
+  it 'has a valid tag' do
+    FactoryGirl.create(:tag).should be_valid
+  end
+
+  it 'should have a name column' do
+    should have_db_column(:name)
+  end
+
+  it 'should be invalid without a name' do
+    FactoryGirl.build(:tag, name: nil).should be_invalid
+  end
+
+  it 'should have many categories' do
+    should have_many(:categories)
+  end
+
+  it 'should have many restaurants' do
+    should have_many(:restaurants)
+  end
+
 end
