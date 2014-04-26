f84d50d0aadd6828637eca6e35199fde95c24e23
Jake Nations <jnations1214@gmail.com>
2014-04-26 13:30:44 -0500

    phot gallery working and playing nice with the map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require gallery/galleria
 //= require gallery/galleria/classic
 //= require_tree .
+var initGallery = function(){
 Galleria.loadAssetTheme('classic');
 Galleria.configure({
     imageCrop: true,
@@ -25,3 +26,4 @@ Galleria.configure({
     // if pic can't be loaded use this one as fallback
     dummy: '/assets/photos/dummy.png'});
 Galleria.run('#galleria');
+};
